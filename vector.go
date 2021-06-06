07b83d9e51726ee9bc97dfeb8ceaddd1ca59ad91
Konstantin Voykov <koykov0@gmail.com>
2021-06-07 00:10:26 +0300

    Implement Bytes()/String() methods to reassembly url from vector.

--- vector.go
+++ vector.go
@@ -61,6 +61,65 @@ func (vec *Vector) ParseCopyStr(s string) error {
 	return vec.parse(fastconv.S2B(s), true)
 }
 
+// Bytes reassembles the vector into a valid URL bytes array.
+func (vec *Vector) Bytes() []byte {
+	// Bytes uses internal buffer as destination array to assemble the URL. So we need to save current length of the
+	// buffer and use it further as offset.
+	offset := vec.BufLen()
+
+	if scheme := vec.SchemeBytes(); len(scheme) > 0 {
+		vec.BufAppend(scheme)
+		vec.BufAppend(bSchemaSep)
+	} else if vec.Slashes() {
+		vec.BufAppend(bSlashes)
+	}
+
+	if username := vec.UsernameBytes(); len(username) > 0 {
+		vec.BufAppend(username)
+		if password := vec.PasswordBytes(); len(password) > 0 {
+			vec.BufAppend(bColon)
+			vec.BufAppend(password)
+		}
+		vec.BufAppend(bAt)
+	}
+
+	if hostname := vec.HostnameBytes(); len(hostname) > 0 {
+		vec.BufAppend(hostname)
+		if port := vec.getByIdx(idxPort); port.Value().Limit() > 0 {
+			vec.BufAppend(bColon)
+			vec.BufAppend(port.Bytes())
+		}
+	}
+
+	if path := vec.PathBytes(); len(path) > 0 {
+		if path[0] != '/' {
+			vec.BufAppend(bSlash)
+		}
+		vec.BufAppend(path)
+	}
+
+	if query := vec.QueryBytes(); len(query) > 0 {
+		if query[0] != '?' {
+			vec.BufAppend(bQM)
+		}
+		vec.BufAppend(query)
+	}
+
+	if hash := vec.HashBytes(); len(hash) > 0 {
+		if hash[0] != '#' {
+			vec.BufAppend(bHash)
+		}
+		vec.BufAppend(hash)
+	}
+
+	return vec.Buf()[offset:]
+}
+
+// String reassembles the vector into a valid URL string.
+func (vec *Vector) String() string {
+	return fastconv.B2S(vec.Bytes())
+}
+
 // Get scheme node.
 func (vec *Vector) Scheme() *vector.Node {
 	return vec.getByIdx(idxScheme)
