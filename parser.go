b91b46ea4f755bd8ce16786cd93137963c4e7586
Konstantin Voykov <koykov0@gmail.com>
2023-10-08 22:49:43 +0300

    Query parser: fix infinite loop bug on bad query prefix.

--- parser.go
+++ parser.go
@@ -309,6 +309,9 @@ func (vec *Vector) parseQueryParams(query *vector.Node) {
 		kv, k, v    []byte
 		root, node  *vector.Node
 	)
+	if origin[0] == '&' {
+		offset++
+	}
 	for {
 		kv, k, v = nil, nil, nil
 
